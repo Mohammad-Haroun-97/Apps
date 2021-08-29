4e065e9cfda4b6ddb98556cdbda29b92397e7b95
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-08-29 11:03:55 +0300

    Finish lab01

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render(){
+        return(
+            <>
+           <h2> Horned Beasts</h2>
+
+            </>
+        )
+    }
+}
+
+export default Header;
---
+++ src/components/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HornedBeast1 from './HornedBeast1'
+import HornedBeast2 from './HornedBeast2'
+
+class Main extends React.Component{
+    render(){
+        return(
+            <>
+            
+           <HornedBeast1/>
+           <HornedBeast2/>
+           
+            </>
+        )
+    }
+}
+
+export default Main;
