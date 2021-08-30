42ac7d1fd9a03c73b4982a888e08e3f90aca2e4c
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-08-30 16:58:36 +0300

    Finish Lab 02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,45 +1,47 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfVotes: 0,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            numberOfVotes : 0
-        }
-    }
+  incrementNumberOfVotes = () => {
+    this.setState({
+      numberOfVotes: this.state.numberOfVotes + 1,
+    });
+  };
 
-    incrementNumberOfVotes = () => {
-        this.setState({
-            numberOfVotes : this.state.numberOfVotes + 1  
-        })
-    }
+  render() {
+    return (
+      <>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={this.props.imgUrl}  alt="HornedBeast"
+          title={this.props.title}
+          onClick={this.incrementNumberOfVotes} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+            {this.props.description}
+            <ul>
+          <li>Number of Votes : {this.state.numberOfVotes}</li>
+          <li>{this.props.keyword}</li>
+          <li>{this.props.horns}</li>
+        </ul>
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
 
-
-
-    render(){
-        return(
-            <>
-
-
-
-            <h2> {this.props.title} </h2>
-
-         <img src={this.props.imgUrl}  alt="HornedBeast"  title = {this.props.title} onClick={this.incrementNumberOfVotes} ></img>
-
-         <p>
-         {this.props.description}
-         </p>
-
-         <ul>
-             <li>Number of Votes : {this.state.numberOfVotes}</li>
-             <li>{this.props.keyword}</li>
-             <li>{this.props.horns}</li>
-         </ul>
-
-            </>
-        )
-    }
+       
+      </>
+    );
+  }
 }
 
 export default HornedBeast;
