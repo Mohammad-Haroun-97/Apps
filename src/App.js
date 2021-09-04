7454b94a7247254de42de70a41073af2cab335f0
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-04 09:17:18 +0200

    finihs lab04

--- src/App.js
+++ src/App.js
@@ -1,77 +1,104 @@
-import React from "react";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import Main from "./components/main";
-import data from "./data.json";
-import SelectedBeats from "./components/SelectedBeast.js";
-import Form from "./components/form";
+import React from 'react'
+import HornedBeast from './components/HornedBeast';
+import Header from './components/header';
+import Footer from './components/footer';
+import Main from './components/main';
+import data from './data.json'
+import SelectedBeast from './components/SelectedBeast';
+import  Form  from './components/form';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false,
-    };
+
+
+class main extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state={
+      show:false,
+      data : data,
+      
+      
+    }
   }
-  showFun = () => {
+
+  showFun=()=>{
     this.setState({
-      show: true,
-    });
-  };
-  hideFun = () => {
+      show : true
+    })
+  }
+
+  hideFun=()=>{
     this.setState({
       show: false,
-    });
-  };
+    })
+  }
 
-  filter = (event) => {
-    event.preventDefault(); 
-    const arrSpecial = data.filter(
-      (item) =>
-        event.target.value == item.horns || event.target.value == "All"
-    );
+  filter = event =>{
+    event.preventDefault();            
+    const arrFilter = data.filter(item=>event.target.value == item.horns || event.target.value =='All' )
     this.setState({
-      searchQuery: event.target.value,
-      data: arrSpecial,
-    });
+      
+      data: arrFilter
+    })
+    console.log(arrFilter);
     
-  };
+    
+  }
 
-  update= (title,img_url,description,horns) => {
+
+
+  update= (title,image_url,description,horns) => {
     this.setState({
       title:title,
-      img_url:img_url,
+      image_url:image_url,
       description:description,
-      horns:horns
+      horns:horns,
+      
       
     })
   }
 
-  render() {
-    return (
+
+
+  render(){
+
+    return(
       <>
-        <SelectedBeats
-          showFun={this.showFun}
-          hideFun={this.hideFun}
-          state={this.state.show}
-        />
-        <Header />
-        <Form filter={this.filter} />
+     <Header/>
 
-        <Main
-          data={data}
-          showFun={this.showFun}
-          hideFun={this.hideFun}
-          state={this.state.show}
-          update={this.update}
+     <Form filter={this.filter}/>
 
-          // showFun={this.showFun} hideFun={this.hideFun} state={this.state.show}
-        />
+     <Main  data={this.state.data}
+      showFun={this.showFun} 
+      hideFun={this.hideFun} 
+      update={this.update}
+      state={this.state}
+      />
+     
+     
 
-        <Footer />
+     <SelectedBeast 
+     showFun={this.showFun} 
+     hideFun={this.hideFun}
+     state={this.state}
+     update={this.update}  
+     
+     title={this.state.title}
+     
+     image_url={this.state.image_url}
+
+     description={this.state.description}
+     horns={this.state.horns}
+
+     />
+
+     <Footer/>
+
+
+
       </>
     );
   }
 }
 
-export default App;
+export default main
--- src/components/main.js
+++ src/components/main.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import HornedBeast from './HornedBeast'
-import Col from 'react-bootstrap/Col';
+
 
 
 class Main extends React.Component{
-    render(){
-        return(
-            <>
-            
-           
-           
-
-           {this.props.data.map((item ,i)=>{
-        return <>
-     
-     <Col xs ={{ order: 1 }}><HornedBeast title={item.title} imgUrl={item.image_url} description={item.description } keyword={item.keyword}  horns={item.horns}
-            showFun={this.props.showFun} hideFun={this.props.hideFun}  i={i}  />
-            
-            </Col>
-
-
-            
-          </>
-      })}
+  render(){
+    return(
+        <>
+        
        
+       {this.props.data.map(item =>{
+    return <>
+ 
+<HornedBeast title={item.title} image_url={item.image_url} description={item.description } horns={item.horns}
+showFun={this.props.showFun}
+hideFun={this.props.hideFun}
+  update={this.props.update} 
+         />
+        
        
-     </>
-    )
 
-  }
+
+        
+      </>
+  })}
+   
+   
+ </>
+)
+
+}
 }
 export default Main;
